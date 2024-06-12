0c00b81209bcf03c5cd5275aebf9e6d0c4007c71
TAJ-32 <sartajs2002@gmail.com>
2024-06-11 20:33:06 -0400

    Added some router stuff but URLs not changing

--- frontend/indian-music-react/src/index.js
+++ frontend/indian-music-react/src/index.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './pages/App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import App from "../src/pages/App.js"
+import Home from "../src/pages/Home.js"
+import Login from "../src/pages/Login.js"
+import Layout from "../src/pages/Layout.js"
+
+export default function MyApp() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+        </Route>
+      </Routes>
+    </Router>
+  )
+
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MyApp />
   </React.StrictMode>
 );
 
--- frontend/indian-music-react/src/pages/App.js
+++ frontend/indian-music-react/src/pages/App.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'; //useEffect
 import Home from './Home.js';
 import '../styles/App.css';
 //import Login from './components/Login.js';
-//import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 //const clientId = '15a7c37cb874416fa0e4a9580f29435e';
 //const redirectUri = 'http://localhost:3000';
